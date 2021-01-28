0ea39cfb12cfca08509308e6e315640319630986
GitHub Action <action@github.com>
2021-01-28 10:04:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/terezo/goodformatterv2/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/terezo/goodformatterv2/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.terezo.goodformatterv2.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.terezo.goodformatterv2.MyBundle
 
 class MyApplicationService {
 
